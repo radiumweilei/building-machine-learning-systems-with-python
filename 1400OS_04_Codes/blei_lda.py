6f047a39449d07d16e2491765830a1d61f902b47
weilei <weilei@wusongtech.com>
2018-05-16 14:57:17 +0800

    调试通过

--- 1400OS_04_Codes/blei_lda.py
+++ 1400OS_04_Codes/blei_lda.py
@@ -1,21 +1,23 @@
 from __future__ import print_function
-from gensim import corpora, models, similarities
-from mpltools import style
+from gensim import corpora, models
+# from mpltools import style  已集成到 matplotlib
+from matplotlib import style
 import matplotlib.pyplot as plt
 import numpy as np
 from os import path
+
 style.use('ggplot')
 
 if not path.exists('./data/ap/ap.dat'):
     print('Error: Expected data to be present at data/ap/')
 
-corpus = corpora.BleiCorpus('./data/ap/ap.dat', './data/ap/vocab.txt')
+corpus = corpora.BleiCorpus('./data/ap/ap.dat', './data/ap/vocab.txt')  # http://www.cs.columbia.edu/~blei/lda-c/ap.tgz
 model = models.ldamodel.LdaModel(corpus, num_topics=100, id2word=corpus.id2word, alpha=None)
 
-for ti in xrange(84):
+for ti in range(84):
     words = model.show_topic(ti, 64)
-    tf = sum(f for f,w in words)
-    print('\n'.join('{}:{}'.format(w, int(1000.*f/tf)) for f,w in words))
+    tf = sum(f for w, f in words)
+    print('\n'.join('{}:{}'.format(w, int(1000. * f / tf)) for w, f in words))
     print()
     print()
     print()
@@ -24,17 +26,17 @@ thetas = [model[c] for c in corpus]
 plt.hist([len(t) for t in thetas], np.arange(42))
 plt.ylabel('Nr of documents')
 plt.xlabel('Nr of topics')
-plt.savefig('../1400OS_04_01+.png')
+plt.savefig('./1400OS_04_01+.png')
 
 model1 = models.ldamodel.LdaModel(corpus, num_topics=100, id2word=corpus.id2word, alpha=1.)
 thetas1 = [model1[c] for c in corpus]
 
-#model8 = models.ldamodel.LdaModel(corpus, num_topics=100, id2word=corpus.id2word, alpha=1.e-8)
-#thetas8 = [model8[c] for c in corpus]
+# model8 = models.ldamodel.LdaModel(corpus, num_topics=100, id2word=corpus.id2word, alpha=1.e-8)
+# thetas8 = [model8[c] for c in corpus]
 plt.clf()
 plt.hist([[len(t) for t in thetas], [len(t) for t in thetas1]], np.arange(42))
 plt.ylabel('Nr of documents')
 plt.xlabel('Nr of topics')
-plt.text(9,223, r'default alpha')
-plt.text(26,156, 'alpha=1.0')
-plt.savefig('../1400OS_04_02+.png')
+plt.text(9, 223, r'default alpha')
+plt.text(26, 156, 'alpha=1.0')
+plt.savefig('./1400OS_04_02+.png')
--- 1400OS_04_Codes/data/download.sh
+++ 1400OS_04_Codes/data/download.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-wget http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2
-wget http://www.cs.princeton.edu/~blei/lda-c/ap.tgz
+wget http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2  # wiki数据集，大得惊人，15GB大小，要命
+#wget http://www.cs.princeton.edu/~blei/lda-c/ap.tgz  # 该文件找不着了，新的地址如下
+wget http://www.cs.columbia.edu/~blei/lda-c/ap.tgz  # 为避免以后找不着该数据集，已经将该数据集提交到git上了
 tar xzf ap.tgz
