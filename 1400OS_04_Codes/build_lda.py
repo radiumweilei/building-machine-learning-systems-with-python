3d1367c5d7058b188895e08f18b03c238741ed9c
weilei <weilei@wusongtech.com>
2018-05-16 21:57:15 +0800

    调试完 04 build_lad.py, 取消 milk

--- 1400OS_04_Codes/build_lda.py
+++ 1400OS_04_Codes/build_lda.py
@@ -1,42 +1,48 @@
-import nltk.corpus
-import milk
+# import milk  # 报错 SystemError: initialization of _kmeans failed without raising an exception, 换成 from scipy.spatial import distance
 import numpy as np
-import string
-from gensim import corpora, models, similarities
+from gensim import corpora, models
 import sklearn.datasets
 import nltk.stem
 from collections import defaultdict
+from gensim.corpora.textcorpus import TextCorpus
+from scipy.spatial import distance
 
 english_stemmer = nltk.stem.SnowballStemmer('english')
 stopwords = set(nltk.corpus.stopwords.words('english'))
 stopwords.update(['from:', 'subject:', 'writes:', 'writes'])
-class DirectText(corpora.textcorpus.TextCorpus):
+
+
+class DirectText(TextCorpus):
     def get_texts(self):
         return self.input
+
     def __len__(self):
         return len(self.input)
-dataset = sklearn.datasets.load_mlcomp("20news-18828", "train", 
-                                       mlcomp_root='../data')
+
+
+dataset = sklearn.datasets.load_mlcomp("20news-18828", "train", mlcomp_root='../data')
 otexts = dataset.data
 texts = dataset.data
 
 texts = [t.decode('utf-8', 'ignore') for t in texts]
 texts = [t.split() for t in texts]
-texts = [map(lambda w: w.lower(), t) for t in texts]
-texts = [filter(lambda s : not len(set("+-.?!()>@012345689") & set(s)), t) for t in texts]
-texts = [filter(lambda s : (len(s) > 3) and (s not in stopwords), t) for t in texts]
-texts = [map(english_stemmer.stem,t) for t in texts]
+texts = [list(map(lambda w: w.lower(), t)) for t in texts]
+texts = [list(filter(lambda s: not len(set("+-.?!()>@012345689") & set(s)), t)) for t in texts]
+texts = [list(filter(lambda s: (len(s) > 3) and (s not in stopwords), t)) for t in texts]
+
+texts = [list(map(english_stemmer.stem, t)) for t in texts]
 usage = defaultdict(int)
 for t in texts:
     for w in set(t):
         usage[w] += 1
-limit = len(texts)/10
+limit = len(texts) / 10
 too_common = [w for w in usage if usage[w] > limit]
 too_common = set(too_common)
-texts = [filter(lambda s : s not in too_common, t) for t in texts]
+texts = [list(filter(lambda s: s not in too_common, t)) for t in texts]
 
 corpus = DirectText(texts)
 dictionary = corpus.dictionary
+
 try:
     dictionary['computer']
 except:
@@ -44,17 +50,22 @@ except:
 
 model = models.ldamodel.LdaModel(corpus, num_topics=100, id2word=dictionary.id2token)
 
-thetas = np.zeros((len(texts),100))
-for i,c in enumerate(corpus):
-    for ti,v in model[c]:
-        thetas[i,ti] += v
-distances = milk.unsupervised.pdist(thetas)
-large = distances.max() + 1
-for i in xrange(len(distances)): distances[i,i] = large
+thetas = np.zeros((len(texts), 100))
+for i, c in enumerate(corpus):
+    for ti, v in model[c]:
+        thetas[i, ti] += v
 
-print otexts[1]
-print
-print
-print
-print otexts[distances[1].argmin()]
+# distances = milk.unsupervised.pdist(thetas)
+# large = distances.max() + 1
+# for i in range(len(distances)): distances[i, i] = large
 
+dis = distance.pdist(thetas)
+large = dis.max() + 1
+for i in range(len(dis)):
+    dis[i] = large
+
+print(otexts[1])
+print()
+print()
+print()
+print(otexts[dis[1].argmin()])
