730717209e6eeec86b2928881c668106827633c3
weilei <weilei@wusongtech.com>
2018-05-17 09:32:06 +0800

    04 wikitopics.py 步骤整理完成

--- 1400OS_04_Codes/wikitopics.py
+++ 1400OS_04_Codes/wikitopics.py
@@ -3,18 +3,27 @@ import numpy as np
 import logging, gensim
 
 # TODO 涉及14G的语料, 整体运行估计要10+小时, 目前没调试
+# 1. 下载语料(数十小时): wget http://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2
+# 2. 构建索引(数小时): python -m gensim.scripts.make_wiki enwiki-latest-pages-articles.xml.bz2 wiki_en_output
+# 然后再运行后续代码
 
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 id2word = gensim.corpora.Dictionary.load_from_text('data/wiki_en_output_wordids.txt')
 mm = gensim.corpora.MmCorpus('data/wiki_en_output_tfidf.mm')
+
+# 3. 建模型(数小时)
 model = gensim.models.ldamodel.LdaModel(
     corpus=mm,
     id2word=id2word,
     num_topics=100,
     update_every=1,
     chunksize=10000,
-    passes=1)
+    passes=1)  # 数小时
 model.save('wiki_lda.pkl')
+
+# 4. 模型应用
+# model = gensim.models.ldamodel.LdaModel.load('wiki_lda.pkl')  # 模型保存后，后续使用可以直接加载，省去 1, 2, 3步
+
 topics = [model[doc] for doc in mm]
 lens = np.array([len(t) for t in topics])
 print(np.mean(lens <= 10))
